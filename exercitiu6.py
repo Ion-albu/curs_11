80287006e94ff7e38b7cf879315314dd94b3bf45
ion <ionalbu0611@gmail.com>
2025-06-20 01:36:40 +0300

    exercitiu6

---
+++ exercitiu6.py
@@ -0,0 +1,9 @@
+# Exercițiul 6: Extragerea unui interval
+# Condiția: Dintr-un tuplu cu numerele de la 1 la 10,
+# extrage un sub-tuplu cu numerele de la poziția 2 la 6 (inclusiv).
+
+numere = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# Scrie codul pentru extragerea sub-tuplului
+sub_tuplu = numere[2:7]
+print(f"numerele extrase de la 2 la 6 inclusib sunt:",sub_tuplu)
