7613417c4c2e98fc9c4c60545888882066fb17ad
ion <ionalbu0611@gmail.com>
2025-06-20 01:38:34 +0300

    exercitiu7

---
+++ exercitiu7.py
@@ -0,0 +1,9 @@
+# Exercițiul 7: Indexare inversă
+# Condiția: Dintr-un tuplu cu litere, accesează a treia literă de la sfârșitul
+# și a cincea literă de la începutul tuplului.
+
+litere = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+a_treia_litera = litere[-3]
+a_cincea_litera = litere[-6]
+print(f"a treia lietara de la sfarsitul tuplului este:",a_treia_litera)
+print(f"a cincea litera de la inceput este:",a_cincea_litera)
