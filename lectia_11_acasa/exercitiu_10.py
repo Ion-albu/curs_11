02ed315d3830db9193758ed0de93bdd75b65cb8a
ion <ionalbu0611@gmail.com>
2025-06-23 14:47:27 +0300

    am adaugat ex 10

---
+++ lectia_11_acasa/exercitiu_10.py
@@ -0,0 +1,17 @@
+"""
+Task: Creați o funcție cu numele "task_10" care are ca argument numele și vârsta unei persoane ca argumente poziționale și orașul ca un argument opțional,
+apoi returnează o descriere a persoanei în forma "Nume: *nume*, Varsta: *varsta*, Oras: *oras*".
+Exemplu: Pentru numele "Ana", vârsta 32 și orașul "București" rezultatul va fi "Nume: Ana, Varsta: 32, Oras: București"
+"""
+
+# CODUL TĂU VINE MAI JOS:
+# print("Ex 10:")
+def task_10(nume, varsta,oras = "Necunoscut"):
+    return f"Nume: {nume}, varsta: {varsta}, Orasul :{oras}"
+
+
+nume = input("Dati un nume: ")
+varsta = int(input("Dati varsta: "))
+oras = input("Dati orasul (optional): ")
+print(task_10(nume, varsta, oras))
+# CODUL TĂU VINE MAI SUS:
