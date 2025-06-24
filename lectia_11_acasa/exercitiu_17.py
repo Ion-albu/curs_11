49dd267834fceaacb7292fb9af8e3cb9248d221a
ion <ionalbu0611@gmail.com>
2025-06-24 15:30:49 +0300

    am adaugat ex 17

---
+++ lectia_11_acasa/exercitiu_17.py
@@ -0,0 +1,13 @@
+"""
+Task: Creați o funcție cu numele "task_17" care acceptă un string și returnează numărul de cuvinte din string.
+Exemplu: Pentru string-ul "Hello, World!" rezultatul va fi 2
+"""
+
+# CODUL TĂU VINE MAI JOS:
+# print("Ex 17:")
+def task_17(str):
+    return len(str.split())
+
+str = input("Dati un string: ")
+print(task_17(str))
+# CODUL TĂU VINE MAI SUS:
