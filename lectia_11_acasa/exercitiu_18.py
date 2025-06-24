a5f0066fc48ff35112265e593cc192e4e8afc13d
ion <ionalbu0611@gmail.com>
2025-06-24 16:20:28 +0300

    am adaugat ex 18

---
+++ lectia_11_acasa/exercitiu_18.py
@@ -0,0 +1,13 @@
+"""
+Task: Creați o funcție cu numele "task_18" care acceptă un număr ce reprezintă temperatura în grade Celsius și returnează temperatura în grade Fahrenheit.
+Exemplu: Pentru temperatura 0 rezultatul va fi 32.0
+"""
+
+# CODUL TĂU VINE MAI JOS:
+# print("Ex 18:")
+def task_18(temp):
+    return temp * 9 / 5 + 32
+
+temp = float(input("Dati temperatura in C: "))
+print(task_18(temp))
+# CODUL TĂU VINE MAI SUS:
