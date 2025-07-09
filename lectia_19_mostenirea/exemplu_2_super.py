33d130cd1179d509847b87dc6a212e2f3831cce1
ion <ionalbu0611@gmail.com>
2025-07-09 15:39:12 +0300

    am adaugat modificarea cu super()

---
+++ lectia_19_mostenirea/exemplu_2_super.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self, nume, varsta, greutate):
+        self.nume = nume
+        self.varsta = varsta
+        self.greutate = greutate
+    def mananca(self):
+        self.greutate += 0.2
+class Pisica(Animal):
+    def __init__(self, nume, varsta, greutate, culoare_blana):
+        super().__init__(nume, varsta,greutate)
+        self.culoare_blana = culoare_blana
+    def zgaraie(self):
+        return "eu zgarai"
+    def descriere(self):
+        return f"numele: {self.nume} are varsta de {self.varsta} si greutatea de {self.greutate} kg"
+    def __str__(self):
+        return f"numele:{self.nume}, varsta:{self.varsta}, greutatea {self.greutate}"
+obj_pisica = Pisica("tom", 15, 20, "alba")
+print(obj_pisica.descriere())
+print(obj_pisica)
+print(obj_pisica.nume)
+print(obj_pisica.culoare_blana)
